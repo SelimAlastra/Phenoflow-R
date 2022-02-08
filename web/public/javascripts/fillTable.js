26538b147f61cf5e25213f5d123cbf54a0f3f71a
Selim Alastra <selim.alastra@kcl.ac.uk>
2022-02-08 01:43:54 +0000

    Updates dataSetGenerator

--- web/public/javascripts/fillTable.js
+++ web/public/javascripts/fillTable.js
@@ -1,17 +1,28 @@
 function fillTable() {
     let dataCodes = document.getElementById("dataCodes").value;
     //remove white spaces from codes
-    dataCodes = dataCodes.replace(/\s/g, '');
-    let codesArray = dataCodes.split(',');
+    // dataCodes = dataCodes.replace(/\s/g, '');
+    let codesArray = dataCodes.split(' ');
+
+    if (dataCodes.length == 0){
+        return
+    }
 
     const numberOfPatients = document.getElementById("numberOfPatients").value;
-    const minimumDate = new Date(1922, 0, 1);
+    const minimumDate = new Date(1920, 0, 1);
+    
+    //Taken from
+    //https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+    function randomIntFromInterval(min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
+      }
 
     //Taken from
     //https://stackoverflow.com/questions/9035627/elegant-method-to-generate-array-of-random-dates-within-two-dates
     function randomDate(start, end) {
         return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
     }
+      
     
     //Taken from
     //https://bobbyhadz.com/blog/javascript-format-date-yyyy-mm-dd
@@ -33,9 +44,6 @@ function fillTable() {
         return formatDate(d)
     }
 
-    let randomCode;
-    let birthDate;
-    let lastEncouter;
     let contents = `
         <thead>
             <tr>
@@ -48,19 +56,40 @@ function fillTable() {
         <tbody>
         `;
 
-    
+
     for (let i = 1; i <= numberOfPatients; i++){
-        randomCode =  codesArray[Math.floor(Math.random()*codesArray.length)];
+        randomCodes = "\""
+        codeDates = []
         birthDate = randomDate(minimumDate, new Date());
-        birthDateFormated = getRandomFormatedDate(birthDate);
-        lastEncouterFormated = getRandomFormatedDate(randomDate(birthDate, new Date()));
-        //last encouter date should be after the birth date.
+        birthDateFormated = (formatDate(birthDate));
+        numberOfCodes = randomIntFromInterval(1, codesArray.length)
+        //random but must be 1 or less that the number of codes in the array
+        for (let i = 1; i <= numberOfCodes; i++){
+            let codeDate;
+            console.log(birthDate)
+            codeDate = randomDate(birthDate, new Date());
+            codeDates.push(codeDate);
+            //must be before the birthdate
+            codeDateFormated = formatDate(codeDate);
+            randomCode = codesArray[Math.floor(Math.random()*codesArray.length)];
+            if(i == 1){
+                randomCodes += "("+randomCode+","+codeDateFormated+")";
+            }
+            else{
+                randomCodes += ","+"("+randomCode+","+codeDateFormated+")";
+            }
+            
+        }
+        randomCodes += "\""
+        lastEncounterDateFormated = formatDate(new Date(Math.max.apply(null, codeDates)))
+        //is the latest date that a code was assigned to a certain patient
+
         contents += `
             <tr>
                 <td>${i}</td>
-                <td>${birthDateFormated} </td>
-                <td>${randomCode}</td>
-                <td>${lastEncouterFormated}</td>
+                <td>${birthDateFormated}</td>
+                <td>${randomCodes}</td>
+                <td>${lastEncounterDateFormated}</td>
             </tr>
         `;
 
