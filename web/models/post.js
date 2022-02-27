dbc309cb3495ec570d5d45ae79948ae500deafb0
SelimAlastra <alastraselim28@yahoo.fr>
2022-02-27 17:03:16 +0000

    Let authors delete forums posts and comments

--- web/models/post.js
+++ web/models/post.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.hasMany(models.answer, {foreignKey: 'postId'})
+      this.hasMany(models.answer, {foreignKey: 'postId', onDelete:'CASCADE', hooks:true})
     }
   }
   post.init({
