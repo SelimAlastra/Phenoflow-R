027f7ddd68e9bbe6e3832b4a53494eaa03756360
SelimAlastra <alastraselim28@yahoo.fr>
2022-03-09 20:48:24 +0000

    Removes iframes from defineByAPI

--- web/public/javascripts/defineAPI.js
+++ web/public/javascripts/defineAPI.js
@@ -1,5 +1,3 @@
-
-
 function sendPostRequest(endpoint, body, callback, contentType='application/json') {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/phenoflow/" + endpoint, true);
@@ -18,53 +16,40 @@ function sendPostRequest(endpoint, body, callback, contentType='application/json
       }
     };
     xhr.send(body);
-  }
-  
-
-function getCSVs() {
-    return [
-        {"filename":"listA_system.csv",
-        "content": [
-        {"ICD-10 code": "123", "description": "TermA TermB"},
-        {"ICD-10 code": "234", "description": "TermA TermC"},
-        {"ICD-10 code": "345", "description": "TermD TermE"}
-        ]},
-        {"filename":"listB_system.csv",
-        "content": [
-        {"SNOMED code": "456", "description": "TermF TermG"},
-        {"SNOMED code": "567", "description": "TermF TermH"},
-        {"SNOMED code": "678", "description": "TermI TermJ"}
-        ]},
-        {"filename":"listC_system.csv",
-        "content": [
-        {"SNOMED code": "456", "description": "TermK TermL"},
-        {"SNOMED code": "567", "description": "TermK TermL"},
-        {"SNOMED code": "678", "description": "TermK TermL"}
-        ]},
-        {"filename":"listD_system.csv",
-        "content": [
-        {"SNOMED code": "456", "description": "TermM TermN"},
-        {"SNOMED code": "567", "description": "TermM TermN"},
-        {"SNOMED code": "678", "description": "TermM TermN"}
-        ]}
-    ]
 }
 
-function createImportedCodeListPhenotype(csvs,name,userName,about,callback=function(response){}){
-    sendPostRequest("importer/importCodelists", JSON.stringify({"csvs": getCSVs(), "name":"test", "about":"test", "userName":"test"}), callback);
-    var data = new FormData();
-    data.append("implementation", file);
+
+function createImportedCodeListPhenotype(csvs,name,about,userName,callback=function(response){}){
+    return sendPostRequest("importer/importCodelists", JSON.stringify({"csvs": csvs, "name": name, "about": about, "userName": userName}), callback);
 }
 
 
 function getImportedCodeListData(){
-    let phenotypeName = document.getElementById("phenotypeName").value;
-    let phenotypeDescription = document.getElementById("phenotypeDescription").value;
-    let phenotypeUsername = "martinchapman";
-    let CSVs = document.getElementById("codeLists").files[0];
-    console.log(phenotypeName)
-    console.log(phenotypeDescription)
-    console.log(phenotypeUsername)
-    console.log(CSVs)
-    createImportedCodeListPhenotype(CSVs,phenotypeName,phenotypeUsername,phenotypeDescription)
+  const csvs = [
+    {"filename":"listA_system.csv",
+    "content": [
+    {"ICD-10 code": "123", "description": "TermA TermB"},
+    {"ICD-10 code": "234", "description": "TermA TermC"},
+    {"ICD-10 code": "345", "description": "TermD TermE"}
+    ]},
+    {"filename":"listB_system.csv",
+    "content": [
+    {"SNOMED code": "456", "description": "TermF TermG"},
+    {"SNOMED code": "567", "description": "TermF TermH"},
+    {"SNOMED code": "678", "description": "TermI TermJ"}
+    ]},
+    {"filename":"listC_system.csv",
+    "content": [
+    {"SNOMED code": "456", "description": "TermK TermL"},
+    {"SNOMED code": "567", "description": "TermK TermL"},
+    {"SNOMED code": "678", "description": "TermK TermL"}
+    ]},
+    {"filename":"listD_system.csv",
+    "content": [
+    {"SNOMED code": "456", "description": "TermM TermN"},
+    {"SNOMED code": "567", "description": "TermM TermN"},
+    {"SNOMED code": "678", "description": "TermM TermN"}
+    ]}
+]
+    createImportedCodeListPhenotype(csvs,"Imported code list","Imported code list", "martinchapman")
 }
