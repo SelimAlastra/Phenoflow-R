c33d9c821b7957e969d401da1fa871b141d6d98c
Selim Alastra <selim.alastra@kcl.ac.uk>
2022-03-11 01:36:47 +0000

    Adds help to the defineAPI page

--- web/public/javascripts/defineAPI.js
+++ web/public/javascripts/defineAPI.js
@@ -1,55 +1,61 @@
-function sendPostRequest(endpoint, body, callback, contentType='application/json') {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/phenoflow/" + endpoint, true);
-    if (contentType) xhr.setRequestHeader('Content-Type', contentType);
-    const idToken = localStorage.getItem("id_token");
-    if (idToken) xhr.setRequestHeader("Authorization", "Bearer " + idToken);
-    xhr.onreadystatechange = function() {
-      if(xhr.readyState === XMLHttpRequest.DONE) {
-        var status = xhr.status;
-        if (status === 0 || (status >= 200 && status < 400)) {
-          callback(xhr.response);
-        } else {
-          console.log(xhr.statusText);
-          callback(null);
-        }
-      }
-    };
-    xhr.send(body);
-}
+// function sendPostRequest(endpoint, body, callback, contentType='application/json') {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("POST", "/phenoflow/" + endpoint, true);
+//     if (contentType) xhr.setRequestHeader('Content-Type', contentType);
+//     const idToken = localStorage.getItem("id_token");
+//     if (idToken) xhr.setRequestHeader("Authorization", "Bearer " + idToken);
+//     xhr.onreadystatechange = function() {
+//       if(xhr.readyState === XMLHttpRequest.DONE) {
+//         var status = xhr.status;
+//         if (status === 0 || (status >= 200 && status < 400)) {
+//           callback(xhr.response);
+//         } else {
+//           console.log(xhr.statusText);
+//           callback(null);
+//         }
+//       }
+//     };
+//     xhr.send(body);
+// }
 
 
-function createImportedCodeListPhenotype(csvs,name,about,userName,callback=function(response){}){
-    return sendPostRequest("importer/importCodelists", JSON.stringify({"csvs": csvs, "name": name, "about": about, "userName": userName}), callback);
-}
+// function createImportedCodeListPhenotype(csvs,name,about,userName,callback=function(response){}){
+//     return sendPostRequest("importer/importCodelists", JSON.stringify({"csvs": csvs, "name": name, "about": about, "userName": userName}), callback);
+// }
 
 
-function getImportedCodeListData(){
-  const csvs = [
-    {"filename":"listA_system.csv",
-    "content": [
-    {"ICD-10 code": "123", "description": "TermA TermB"},
-    {"ICD-10 code": "234", "description": "TermA TermC"},
-    {"ICD-10 code": "345", "description": "TermD TermE"}
-    ]},
-    {"filename":"listB_system.csv",
-    "content": [
-    {"SNOMED code": "456", "description": "TermF TermG"},
-    {"SNOMED code": "567", "description": "TermF TermH"},
-    {"SNOMED code": "678", "description": "TermI TermJ"}
-    ]},
-    {"filename":"listC_system.csv",
-    "content": [
-    {"SNOMED code": "456", "description": "TermK TermL"},
-    {"SNOMED code": "567", "description": "TermK TermL"},
-    {"SNOMED code": "678", "description": "TermK TermL"}
-    ]},
-    {"filename":"listD_system.csv",
-    "content": [
-    {"SNOMED code": "456", "description": "TermM TermN"},
-    {"SNOMED code": "567", "description": "TermM TermN"},
-    {"SNOMED code": "678", "description": "TermM TermN"}
-    ]}
-]
-    createImportedCodeListPhenotype(csvs,"Imported code list","Imported code list", "martinchapman")
+// function getImportedCodeListData(){
+//   const csvs = [
+//     {"filename":"listA_system.csv",
+//     "content": [
+//     {"ICD-10 code": "123", "description": "TermA TermB"},
+//     {"ICD-10 code": "234", "description": "TermA TermC"},
+//     {"ICD-10 code": "345", "description": "TermD TermE"}
+//     ]},
+//     {"filename":"listB_system.csv",
+//     "content": [
+//     {"SNOMED code": "456", "description": "TermF TermG"},
+//     {"SNOMED code": "567", "description": "TermF TermH"},
+//     {"SNOMED code": "678", "description": "TermI TermJ"}
+//     ]},
+//     {"filename":"listC_system.csv",
+//     "content": [
+//     {"SNOMED code": "456", "description": "TermK TermL"},
+//     {"SNOMED code": "567", "description": "TermK TermL"},
+//     {"SNOMED code": "678", "description": "TermK TermL"}
+//     ]},
+//     {"filename":"listD_system.csv",
+//     "content": [
+//     {"SNOMED code": "456", "description": "TermM TermN"},
+//     {"SNOMED code": "567", "description": "TermM TermN"},
+//     {"SNOMED code": "678", "description": "TermM TermN"}
+//     ]}
+// ]
+//     createImportedCodeListPhenotype(csvs,"Imported code list","Imported code list", "martinchapman")
+// }
+
+function getIdToken(){
+  // console.log(localStorage.getItem("id_token"))
+  document.getElementById("idTokenString").innerHTML = localStorage.getItem("id_token")
+  for(let exporterButton of document.getElementsByClassName("idContainer")) exporterButton.style.display = "inline-block";
 }
