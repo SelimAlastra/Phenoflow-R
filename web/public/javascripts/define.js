5b9a5206bb81996e6ed63d98b14988d2888b96a6
SelimAlastra <alastraselim28@yahoo.fr>
2022-03-19 01:38:32 +0000

    fixes major issues in define and edit pages and add some validation

--- web/public/javascripts/define.js
+++ web/public/javascripts/define.js
@@ -1,3 +1,4 @@
+
 function processInputOutputImplementation(create, stepNode, stepId) {
   let inputDoc = stepNode.getElementsByClassName("inputDoc")[0].value;
   let outputDoc = stepNode.getElementsByClassName("outputDoc")[0].value;
@@ -61,7 +62,12 @@ function createOrUpdateWorkflow() {
 }
 
 var inputSteps = 1;
+
 function addStep() {
+  //to find the number of steps
+  let stepNodes = document.getElementById("steps").childNodes;
+  inputSteps = stepNodes.length;
+
   let original = document.getElementById("step1");
   let clone = original.cloneNode(true);
   clone.id = "step" + ++inputSteps;
@@ -74,11 +80,20 @@ function addStep() {
   clone.getElementsByClassName("implementationLanguage")[0].value = "-";
   clone.getElementsByClassName("implementationFileExisting")[0].style.display = "none";
   clone.getElementsByClassName("stepRemove")[0].style.display = "inline";
+
+  // console.log(clone.getElementsByClassName("")
   for(let type of clone.getElementsByClassName("type")) {
     type.id = "step" + inputSteps + type.value;
     type.name = "step" + inputSteps + "type";
   }
-  for(let typeLabel of clone.getElementsByTagName("label")) typeLabel.htmlFor = "step" + inputSteps + typeLabel.className;
+
+  //Change onclick of the addImplementation input as it was not changed
+  clone.getElementsByClassName("addImplementation")[0].setAttribute("onClick", "addImplementation('step" + inputSteps +"'); return false;");
+
+  for(let typeLabel of clone.getElementsByTagName("label")) {
+    // console.log(typeLabel.className.split(" ").pop())
+    // Fix class name 
+    typeLabel.htmlFor = "step" + inputSteps + typeLabel.className.split(" ").pop()}
   original.parentNode.appendChild(clone);
 }
 
@@ -103,4 +118,62 @@ function addImplementation(stepInputId) {
   let implementationFileExisting = clone.getElementsByClassName("implementationFileExisting")?clone.getElementsByClassName("implementationFileExisting")[0]:null;
   if (implementationFileExisting ) implementationFileExisting.remove();
   original.parentNode.appendChild(clone);
+}
+
+
+function defineValidation(){
+  document.getElementById("validationMessage").innerHTML = "";
+  // reset validation message
+  let phenotypeName = document.getElementsByClassName("workflowName")[0].value;
+  let phenotyeDescription = document.getElementsByClassName("workflowAbout")[0].value;
+  let stepNodes = document.getElementById("steps").childNodes;
+  let counter = 0;
+  for (let step of stepNodes){
+    counter++;
+    let inputDescription = step.getElementsByClassName("inputDoc")[0].value;
+    let outputDescription = step.getElementsByClassName("outputDoc")[0].value;
+    let outputExtension = step.getElementsByClassName("outputExtension")[0].value;
+    let stepName = step.getElementsByClassName("name")[0].value;
+    let stepDescription = step.getElementsByClassName("doc")[0].value;
+    let stepType;
+    for (let potentialStepType of step.getElementsByClassName("type")) {
+      if (potentialStepType.checked) stepType = potentialStepType.value;
+    }
+    // console.log(stepType)
+
+    // if (outputExtension != "csv")
+    // { 
+    //   document.getElementById("validationMessage").innerHTML = "Please enter a correct output extension for step number " + counter;
+    // }
+    // if (outputExtension.length == 0)
+    // { 
+    //   document.getElementById("validationMessage").innerHTML = "Please enter the step number " + counter + " output extension";
+    // }
+    // if (outputDescription.length == 0)
+    // { 
+    //   document.getElementById("validationMessage").innerHTML = "Please enter the step number " + counter + " output description";
+    // }
+    // if (inputDescription.length == 0)
+    // { 
+    //   document.getElementById("validationMessage").innerHTML = "Please enter the step number " + counter + " input description";
+    // }
+    let implementationCounter = 0;
+    for (let implementationNode of step.getElementsByClassName("implementation")) {
+      implementationCounter++
+      let implementationFile = implementationNode.getElementsByClassName("implementationFile")[0].files[0];
+      let implementationLanguage = implementationNode.getElementsByClassName("implementationLanguage")[0].value
+      if(!implementationFile){document.getElementById("validationMessage").innerHTML = "The implementation file number "+ implementationCounter + " for step number " + counter;}
+      // console.log(implementationFile)
+      // console.log(implementationLanguage)
+    }
+  }
+
+
+  // if (phenotyeDescription.length == 0){
+  //   document.getElementById("validationMessage").innerHTML = "Please enter a phenotype description";
+  // }
+  // if (phenotypeName.length == 0){
+  //   document.getElementById("validationMessage").innerHTML = "Please enter a phenotype name";
+  // }
+
 }
