e2aab5fcbd0d68d3cf3656e83aa8c4b3efe3d173
Selim Alastra <selim.alastra@kcl.ac.uk>
2022-04-09 04:26:17 +0100

    Adds cypress testing

---
+++ web/test/dataSetGenerator.js
@@ -0,0 +1,22 @@
+// As this page was using javascript functions, they were retaken and adpated to be tested.
+
+const chai = require("chai");
+chai.use(require("chai-http"));
+const server = require("../app");
+const should = chai.should();
+const expect = chai.expect;
+
+function generateTable(){
+    let advancedParams = false
+     
+
+}
+
+
+describe("dataSetGenerator", () => {
+
+
+
+
+
+});
